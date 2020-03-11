ca4b6e98fa20f5bbf66b9fc62630f081637337f6
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-11 11:42:55 +0100

    @Gerglamesh

---
+++ Source/Railway/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Railway
+{
+    //Carlos
+    public class Controller
+    {
+        public List<Train> Trains { get; } = new List<Train>();
+        public List<Station> Stations { get; } = new List<Station>();
+        public List<TimeTable> TimeTables { get; } = new List<TimeTable>();
+        public List<Passenger> Passengers { get; } = new List<Passenger>();
+
+        public Controller()
+        {
+            this.Trains = ObjectBuilder.Trains;
+            this.Stations = ObjectBuilder.Stations;
+            this.TimeTables = ObjectBuilder.TimeTables;
+            this.Passengers = ObjectBuilder.Passengers;
+        }
+
+    }
+}
